10473013ada74e8f75f4dc9be1d2350cb048bd12
grum1n <grumin.el.pastas@gmail.com>
2022-03-29 13:18:55 +0300

    added header navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Menu } from '../../data/Menu';
 import './header.css';
+import * as FaIcons from 'react-icons/fa';
+import LogoImage from '../../images/logo.png';
 
 function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <nav className='header-navigation'>
+                <Link to='/'>
+                    <img src={LogoImage} alt='Manice' className='header-logo-img'/>
+                </Link>
+                <div className='flex-container'>
+                    <ul className='flex-container'>
+                        {Menu.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link to={item.path}>
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <button>
+                        <FaIcons.FaSearch />
+                    </button>
+                    <button className='menu-button'>
+                        <FaIcons.FaBars />
+                    </button>
+                </div>
+            </nav>
+        </header>
     );
 }
 
---
+++ src/data/Menu.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+
+export const Menu = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <FaIcons.FaHome />,
+    cName: 'mobile-text'
+  },
+  {
+    title: 'About',
+    path: '/about',
+    icon: <FaIcons.FaCarAlt />,
+    cName: 'mobile-text'
+  },
+  {
+    title: 'Services',
+    path: '/services',
+    icon: <FaIcons.FaUserLock />,
+    cName: 'mobile-text'
+  },
+  {
+    title: 'News',
+    path: '/news',
+    icon: <FaIcons.FaUserLock />,
+    cName: 'mobile-text'
+  },
+  {
+    title: 'Pages',
+    path: '/pages',
+    icon: <FaIcons.FaUserLock />,
+    cName: 'mobile-text'
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+    icon: <FaIcons.FaUserLock />,
+    cName: 'mobile-text'
+  }
+];
