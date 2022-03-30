ea8354d82487727a03b7f5a9d1b817d80aead3c7
grum1n <grumin.el.pastas@gmail.com>
2022-03-30 21:51:19 +0300

    to page404 added content and style

--- src/pages/public/Page404/index.js
+++ src/pages/public/Page404/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FrameContainer from '../../../components/FrameContainer';
 import './page404.css';
+import Image404 from '../../../images/error404.png';
 
 function Page404() {
     return (
         <FrameContainer>
-            <div className='container'>Page404</div>
+            <div className='page404-box-flex-container'>
+                <div className='page404-box'>
+                    <img src={Image404} alt='image 404' className='page404-box-img'/>
+                    <div className='page404-box-text'>
+                        <h3 className='page404-box-text-h3'>Oh no, something went wrong!</h3>
+                        <p className='page404-box-text-p'>We provide a professional service for private and commercial customers.</p>
+                    </div>
+                </div>
+                <Link to='/' className='page404-box-button'>
+                    Back to Home
+                </Link>
+            </div>
         </FrameContainer>
     );
 }
