dac6852e032ee36d4a493612e33b38679e6717b7
grum1n <grumin.el.pastas@gmail.com>
2022-03-31 12:35:47 +0300

    added content and style to news page

--- src/pages/public/Home/index.js
+++ src/pages/public/Home/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import FrameContainer from '../../../components/FrameContainer';
 import HeroSmall from '../../../components/HeroSmall';
 import Partners from '../../../components/Partners';
+import { NewsData } from '../../../data/NewsData';
+import NewsCard from '../News/NewsCard';
 import ServicesTable from '../Services/ServicesTable';
 import './home.css';
 
@@ -9,9 +11,30 @@ function HomePage() {
     return (
         <FrameContainer>
             <HeroSmall title='Home' />
-            <div className='container'>HomePage</div>
-            <ServicesTable />
+            <div className='container home-component-container'>
+            <h3 className='home-component-container-h3'><span className='home-component-container-span'>Our</span> Services</h3>
+                <ServicesTable />
+            </div>
             <Partners />
+            <div className='container home-component-container'>
+                <h3 className='home-component-container-h3'><span className='home-component-container-span'>Recent</span> News</h3>
+                <div className='newsPage-flex-container'>
+                    {
+                        NewsData.map((newsCard, index) => {
+                            return(
+                                <NewsCard 
+                                    title={newsCard.title} 
+                                    date={newsCard.date}
+                                    category={newsCard.category}
+                                    contentText={newsCard.contentText}
+                                    key={index}
+                                />
+                            );
+                        })
+                    }
+                </div>
+            </div>
+           
         </FrameContainer>
     );
 }
--- src/pages/public/News/index.js
+++ src/pages/public/News/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import FrameContainer from '../../../components/FrameContainer';
 import HeroSmall from '../../../components/HeroSmall';
 import './news.css';
+import NewsCard from './NewsCard';
+import { NewsData } from '../../../data/NewsData';
 
 function NewsPage() {
     return (
         <FrameContainer>
             <HeroSmall title='News' />
-            <div className='container'>NewsPage</div>
+            <div className='container newsPage-flex-container'>
+                {
+                    NewsData.map((newsCard, index) => {
+                        return(
+                            <NewsCard 
+                                title={newsCard.title} 
+                                date={newsCard.date}
+                                category={newsCard.category}
+                                contentText={newsCard.contentText}
+                                key={index}
+                            />
+                        );
+                    })
+                }
+            </div>
         </FrameContainer>
     );
 }
