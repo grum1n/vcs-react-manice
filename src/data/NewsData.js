8dbb4fdcf0712343586431dbc0e964135b7a8281
grum1n <grumin.el.pastas@gmail.com>
2022-03-31 13:05:44 +0300

    added pagination to news page

--- src/data/NewsData.js
+++ src/data/NewsData.js
@@ -34,6 +34,24 @@ export const NewsData = [
         date: 'July 29, 2020',
         category: 'Arts',
         contentText: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.'
+    },
+    {
+        title: 'Reprehenderit in voluptate velit esse cillum',
+        date: 'July 29, 2020',
+        category: 'Arts',
+        contentText: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.'
+    },
+    {
+        title: 'Reprehenderit in voluptate velit esse cillum',
+        date: 'July 29, 2020',
+        category: 'Arts',
+        contentText: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.'
+    },
+    {
+        title: 'Reprehenderit in voluptate velit esse cillum',
+        date: 'July 29, 2020',
+        category: 'Arts',
+        contentText: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa.'
     }
 
 ];
--- src/pages/public/News/index.js
+++ src/pages/public/News/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FrameContainer from '../../../components/FrameContainer';
 import HeroSmall from '../../../components/HeroSmall';
 import './news.css';
+import './pagination.css';
 import NewsCard from './NewsCard';
 import { NewsData } from '../../../data/NewsData';
+import ReactPaginate from 'react-paginate';
 
 function NewsPage() {
+    const [news, setNews] = useState(NewsData.slice(0, 100000));
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const newsPerPage = 6;
+    const pagesVisited = pageNumber * newsPerPage; //
+
+    const displayNews = news
+        .slice(pagesVisited, pagesVisited + newsPerPage)
+        .map((newsCard, index) => {
+        return(
+            <NewsCard 
+                title={newsCard.title} 
+                date={newsCard.date}
+                category={newsCard.category}
+                contentText={newsCard.contentText}
+                key={index}
+            />
+        );
+    });
+
+    const pageCount = Math.ceil(news.length / newsPerPage);
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
+
+    console.log('setNews:', setNews);
     return (
         <FrameContainer>
             <HeroSmall title='News' />
             <div className='container newsPage-flex-container'>
-                {
+                {displayNews}
+                {/* {
                     NewsData.map((newsCard, index) => {
                         return(
                             <NewsCard 
@@ -22,7 +52,18 @@ function NewsPage() {
                             />
                         );
                     })
-                }
+                } */}
+                <ReactPaginate 
+                    previousLabel={'Previous'}
+                    nextLabel={'Next'}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={'paginationBttns'}
+                    previousLinkClassName={'previousBttn'}
+                    nextLinkClassName={'nextBttn'}
+                    disabledClassName={'paginationDisabled'}
+                    activeClassName={'paginationActive'}
+                />
             </div>
         </FrameContainer>
     );
